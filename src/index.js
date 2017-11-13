f3ef985c013744378307c7894e2e2ee1ce18f43d
陶懿臻 <1126341821@qq.com>
2017-11-13 12:51:36 +0800

    dd

--- src/index.js
+++ src/index.js
@@ -15,14 +15,13 @@ render(
   </div>,
   document.getElementById("root")
 );
-
-store.addTodo("Get Coffee");
+store.addTodo("Get Coffee");// 调用todoListmodel添加 Get Coffee
 store.addTodo("Write simpler code");
-store.todos[0].finished = true;
+store.todos[0].finished = true;// 让数据的第一个数据的finished属性为真
 
 setTimeout(() => {
   store.addTodo("Get a cookie as well");
 }, 2000);
 
 // playing around in the console
-window.store = store;
+window.store = store; 
--- src/models/TodoListModel.js
+++ src/models/TodoListModel.js
@@ -5,7 +5,7 @@ import TodoModel from "./TodoModel";
 export default class TodoListModel {
   @observable todos = [];
 
-  @computed
+  @computed // 可以直接被调用
   get unfinishedTodoCount() {
     return this.todos.filter(todo => !todo.finished).length;
   }
@@ -13,5 +13,24 @@ export default class TodoListModel {
   @action
   addTodo(title) {
     this.todos.push(new TodoModel(title));
+    console.log(this.todos);
+    // todos是被包了一层的数组（ObservableArray），
+    //   并且每个数据有值极为push进去的值
+    //   反之为undefined，
   }
 }
+
+class Foo {
+    @observable length = 2;
+    @computed get squared() {
+        return this.length * this.length;
+    }
+    set squared(value) { // 这是一个自动的动作，不需要注解
+        this.length = Math.sqrt(value); // 根号10
+    }
+}
+const ff=new Foo();
+ff.squared=100;// set
+console.log(ff.squared); // get 100
+console.log(ff.length);// 10
+
