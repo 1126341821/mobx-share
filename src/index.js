f51599214744116d6571a03e921c2bf47e54a312
陶懿臻 <1126341821@qq.com>
2017-11-14 21:41:13 +0800

    tyz

--- src/index.js
+++ src/index.js
@@ -6,22 +6,59 @@ import TodoList from "./components/TodoList";
 import TodoListModel from "./models/TodoListModel";
 import TodoModel from "./models/TodoModel";
 
-const store = new TodoListModel();
+// const store = new TodoListModel();
 
-render(
-  <div>
-    <DevTools />
-    <TodoList store={store} />
-  </div>,
-  document.getElementById("root")
-);
-store.addTodo("Get Coffee");// 调用todoListmodel添加 Get Coffee
-store.addTodo("Write simpler code");
-store.todos[0].finished = true;// 让数据的第一个数据的finished属性为真
+// render(
+//   <div>
+//     <DevTools />
+//     <TodoList store={store} />
+//   </div>,
+//   document.getElementById("root")
+// );
+// store.addTodo("Get Coffee");// 调用todoListmodel添加 Get Coffee
+// store.addTodo("Write simpler code");
+// store.todos[0].finished = true;// 让数据的第一个数据的finished属性为真
 
-setTimeout(() => {
-  store.addTodo("Get a cookie as well");
-}, 2000);
+// setTimeout(() => {
+//   store.addTodo("Get a cookie as well");
+// }, 2000);
 
-// playing around in the console
-window.store = store; 
+// // playing around in the console
+// window.store = store; 
+
+
+// import { observer } from "mobx-react";
+// import { observable, computed, action, autorun, toJS } from "mobx";
+
+// var timerData = observable({
+//   secondsPassed: 0
+// });
+
+// setInterval(() => {
+//   timerData.secondsPassed++;
+// }, 1000);
+// let { secondsPassed } = toJS(timerData);
+// @observer
+// class Timer extends React.Component {
+//   render() {
+//     const { secondsPassed } = toJS(timerData);
+//     return (
+//       <div>
+//         <span>Seconds passed: {timerData.secondsPassed} </span>
+//         {/* <span>Seconds passed: {this.props.timerData.secondsPassed} </span> */}
+//         <span>Seconds passed: {this.props.timerData} </span>
+        
+//       </div>)
+//   }
+// };
+
+// console.log(secondsPassed);
+// render(
+//   <div>
+//      <Timer timerData={timerData.secondsPassed}/> 
+//     {/* <Timer timerData={timerData}/> */}
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// /* <Timer timerData={timerData.secondsPassed}/> 不能这么写 */
--- src/models/TodoListModel.js
+++ src/models/TodoListModel.js
@@ -1,4 +1,4 @@
-import { observable, computed, action } from "mobx";
+import { observable, computed, action ,autorun} from "mobx";
 
 import TodoModel from "./TodoModel";
 
@@ -20,17 +20,29 @@ export default class TodoListModel {
   }
 }
 
-class Foo {
-    @observable length = 2;
-    @computed get squared() {
-        return this.length * this.length;
-    }
-    set squared(value) { // 这是一个自动的动作，不需要注解
-        this.length = Math.sqrt(value); // 根号10
-    }
-}
-const ff=new Foo();
-ff.squared=100;// set
-console.log(ff.squared); // get 100
-console.log(ff.length);// 10
+// class Foo {
+//   @observable length = 2;
+//   @computed get squared() {
+//     if (this.length === 0)
+//       throw new Error("Division by zero")
+//     return this.length * this.length;
+//   } 
+// }
+// const ff = new Foo();
+// ff.squared = 0;// set
+// console.log(ff.squared); // get 100
+// console.log(ff.length);// 10
 
+// var numbers = observable([1,2,3]);
+// var numbers2 = observable([4,5,6]);
+// var sum = computed(() => numbers.reduce((a, b) => a + b, 0));
+
+// var disposer = autorun(() => console.log(numbers2));
+
+// // 输出 '6'
+// numbers.push(4);
+// // 输出 '10'
+
+// disposer();
+// numbers.push(5);
+// // 不会再输出任何值。`sum` 不会再重新计算。
